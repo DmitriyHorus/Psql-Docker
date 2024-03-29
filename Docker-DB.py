e17880010b86d8e9a481884bf663071a9dd4b18e
Dmitriy Horus <147636513+DmitriyHorus@users.noreply.github.com>
2024-03-29 13:32:47 +0300

    Update Docker-DB.py

--- Docker-DB.py
+++ Docker-DB.py
@@ -10,9 +10,16 @@ os.system('sudo yum install -y yum-utils device-mapper-persistent-data lvm2')
 os.system('sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo')
 
 # Устанавливаем Docker
-os.system('sudo yum install docker-ce')
+os.system('sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin')
 
 # Запускаем сервис Docker
 os.system('sudo systemctl start docker')
 os.system('sudo systemctl enable docker')
+
+# Создание сети
+os.system('docker network create --subnet 172.20.0.0/16 --ip-range 172.20.240.0/20 postgres')
+
+# Запуск docker-postgres:14
+os.system('sudo docker-compose up -d')
+
 print("Установка Docker и PostgreSQL прошла успешно!")
